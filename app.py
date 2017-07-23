3cc0ceab7bd88cf630f5548b1f62df749fb540c0
linxidd <linxizhangdong@163.com>
2017-07-23 22:12:39 +0800

    创建文章数据库＇

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 #-*- coding=utf-8 -*-
+import os
 from flask import Flask, render_template, request, Markup
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    article_title = db.Column(db.String())
+    article_content = db.Column(db.String())
+    article_time = db.Column(db.DateTime())
+    article_from = db.Column(db.String(64))
+    article_readed = db.Column(db.String())
+    article_wait = db.Column(db.String())
+
+    def __init__(self, article_title, article_content, article_time,
+                 article_from, article_readed, article_wait):
+        self.article_title = article_title
+        self.article_content = article_content
+        self.article_time = article_time
+        self.article_from = article_from
+        self.article_readed = article_readed
+        self.article_wait = article_wait
+
+    def __repr__(self):
+        return '<Article %r>' % self.article_title
 
 
 '''
@@ -26,7 +50,7 @@ def editor():
 
 @app.route('/upload/', methods=['GET', 'POST'])
 def upload():
-    return render_template('article.html', content=Markup(request.form['editorValue']),publish_data=u'2017-10-23 11:34',link=u'新闻')
+    return render_template('article.html', content=Markup(request.form['editorValue']), publish_data=u'2017-10-23 11:34', link=u'新闻')
 
 
 if __name__ == '__main__':
