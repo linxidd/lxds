f963d4549f2d515f8d1fa6593749cf32eb978b90
linxidd <linxizhangdong@163.com>
2017-07-23 17:53:14 +0800

    数据库

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #-*- coding=utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////database.db'
+db = SQLAlchemy(app)
 
 
 '''
@@ -23,7 +26,7 @@ def editor():
 
 @app.route('/upload/', methods=['GET', 'POST'])
 def upload():
-    return render_template('article.html', content=request.form['editorValue'],publish_data=u'2017-10-23 11:34',link=u'新闻')
+    return render_template('article.html', content=Markup(request.form['editorValue']),publish_data=u'2017-10-23 11:34',link=u'新闻')
 
 
 if __name__ == '__main__':
